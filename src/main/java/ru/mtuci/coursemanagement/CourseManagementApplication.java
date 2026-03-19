56e075c194d434afafd3fd7ef3135be46f7438c1
Федоров Максим <fedorov2039@gmail.com>
2026-03-19 21:28:46 +0300

    Fix empty main class in main

---
+++ src/main/java/ru/mtuci/coursemanagement/CourseManagementApplication.java
@@ -0,0 +1,11 @@
+echo 'package ru.mtuci.coursemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseManagementApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CourseManagementApplication.class, args);
+    }
+}' > src/main/java/ru/mtuci/coursemanagement/CourseManagementApplication.java
